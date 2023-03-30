1fc5bfe3eae1d3daeb234a2e43829310f20ccf22
Arthur Rochesso <arthurochesso@gmail.com>
2023-03-30 13:45:23 +0100

    Add project files.

---
+++ Notes/ViewModels/NoteViewModel.cs
@@ -0,0 +1,76 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class NoteViewModel : ObservableObject, IQueryAttributable
+{
+	private Models.Note _note;
+
+	public NoteViewModel()
+	{
+		_note = new Models.Note();
+		SaveCommand = new AsyncRelayCommand(Save);
+		DeleteCommand = new AsyncRelayCommand(Delete);
+	}
+
+	public NoteViewModel(Models.Note note)
+	{
+		_note = note;
+		SaveCommand = new AsyncRelayCommand(Save);
+		DeleteCommand = new AsyncRelayCommand(Delete);
+	}
+
+	public string Text
+	{
+		get => _note.Text;
+		set
+		{
+			if (_note.Text != value)
+			{
+				_note.Text = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public DateTime Date => _note.Date;
+	public string Identifier => _note.Filename;
+	public ICommand SaveCommand { get; private set; }
+	public ICommand DeleteCommand { get; private set; }
+
+	public void Reload()
+	{
+		_note = Models.Note.Load(_note.Filename);
+		RefreshProperties();
+	}
+
+	private async Task Save()
+	{
+		_note.Date = DateTime.Now;
+		_note.Save();
+		await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+	}
+
+	private async Task Delete()
+	{
+		_note.Delete();
+		await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+	}
+
+	void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+	{
+		if (query.ContainsKey("load"))
+		{
+			_note = Models.Note.Load(query["load"].ToString());
+			RefreshProperties();
+		}
+	}
+
+	private void RefreshProperties()
+	{
+		OnPropertyChanged(nameof(Text));
+		OnPropertyChanged(nameof(Date));
+	}
+}
